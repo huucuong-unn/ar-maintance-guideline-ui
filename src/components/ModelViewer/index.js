98d3086b6354a60de1032a0bc64c2791847d656e
CuongLH <lehuucuong270603@gmail.com>
2025-02-21 23:15:12 +0700

    Update version

--- src/components/ModelViewer/index.js
+++ src/components/ModelViewer/index.js
@@ -1,21 +1,21 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+// import React, { Suspense } from 'react';
+// import { Canvas } from '@react-three/fiber';
+// import { OrbitControls, useGLTF } from '@react-three/drei';
 
-function Model({ url }) {
-    const { scene } = useGLTF(url);
-    return <primitive object={scene} dispose={null} />;
-}
+// function Model({ url }) {
+//     const { scene } = useGLTF(url);
+//     return <primitive object={scene} dispose={null} />;
+// }
 
-export default function ModelViewer() {
-    return (
-        <Canvas style={{ height: '500px', width: '100%' }}>
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[0, 10, 5]} intensity={1} />
-            <Suspense fallback={null}>
-                <Model url="file:///D:/Download/mouseclick.glb" />
-                <OrbitControls />
-            </Suspense>
-        </Canvas>
-    );
-}
+// export default function ModelViewer() {
+//     return (
+//         <Canvas style={{ height: '500px', width: '100%' }}>
+//             <ambientLight intensity={0.5} />
+//             <directionalLight position={[0, 10, 5]} intensity={1} />
+//             <Suspense fallback={null}>
+//                 <Model url="file:///D:/Download/mouseclick.glb" />
+//                 <OrbitControls />
+//             </Suspense>
+//         </Canvas>
+//     );
+// }
