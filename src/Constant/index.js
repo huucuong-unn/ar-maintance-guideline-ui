0cf1f4e863c6618501bdd0ff43e84bb7144e9c84
CuongLH <lehuucuong270603@gmail.com>
2025-03-09 14:32:07 +0700

    Change to deploy

--- src/Constant/index.js
+++ src/Constant/index.js
@@ -1,9 +1,10 @@
 // export const host = process.env.REACT_APP_BACKEND_APP_HOST_LOCAL;
 // const host = process.env.REACT_APP_BACKEND_APP_HOST_DEPLOY;
 
+//get all file from host, not just image
 export const getImage = (image) => {
     return `${host}/api/v1/files/${image}`;
 };
 
 const hostImage = process.env.REACT_APP_BACKEND_APP_HOST_DEPLOY;
-export const host = process.env.REACT_APP_BACKEND_APP_HOST_LOCAL;
+export const host = process.env.REACT_APP_BACKEND_APP_HOST_DEPLOY;
